679f41a4e8af0b27ba14e9e6c4396f41654b60c6
Artur Boltrukevich <79936879+artur-bolt@users.noreply.github.com>
2023-03-01 09:30:54 +0100

    Update spark_app_chart_in_cluster_1.py

--- airflow/dags/spark_app_chart_in_cluster_1.py
+++ airflow/dags/spark_app_chart_in_cluster_1.py
@@ -3,8 +3,9 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import Kubernete
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 from datetime import datetime, timedelta
 from airflow.utils.dates import days_ago
+from airflow.models import Variable
 now = datetime.now()
-sparkAppName='airflow-pod-1'
+
 default_args = {
     "owner": "k8s-kubectl",
     "depends_on_past": False,
@@ -21,11 +22,15 @@ dag = DAG(
     schedule_interval=timedelta(days=1),
     tags=['pod','inside','bitnami']
 )
+
+spark_app_name='airflow-pod-1'
+Variable.set("spark_app_name", spark_app_name, dag=dag)
+
 start_pod = KubernetesPodOperator(
     task_id="deploy_helm_pod",
     namespace="operators",
     image="alpine/helm",
-    cmds=['/bin/sh', '-c', 'helm repo add poc https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/charts/ && helm dependency update poc && helm upgrade -i {{ var.value.sparkAppName }} poc/spark-application -f https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/temp/values.yaml --set-string name={{ var.value.sparkAppName }} --wait'],
+    cmds=['/bin/sh', '-c', 'helm repo add poc https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/charts/ && helm dependency update poc && helm upgrade -i {{ var.value.spark_app_name }} poc/spark-application -f https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/temp/values.yaml --set-string name={{ var.value.spark_app_name }} --wait'],
     service_account_name="helm-identity",
     do_xcom_push=False,
     is_delete_operator_pod=True,
@@ -36,7 +41,7 @@ start_pod = KubernetesPodOperator(
 sensor = SparkKubernetesSensor(
     task_id='sensor_helm_app',
     namespace="operators",
-    application_name=sparkAppName,
+    application_name=spark_app_name,
     kubernetes_conn_id="kubernetes_default",
     dag=dag,
     api_group="sparkoperator.k8s.io",

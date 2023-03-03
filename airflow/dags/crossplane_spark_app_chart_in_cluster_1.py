2bbe5b06aa7dfecd4c901310681df6d5fa101463
artur-bolt <artur@opsguru.io>
2023-03-03 13:57:07 +0100

    dags

--- airflow/dags/crossplane_spark_app_chart_in_cluster_1.py
+++ airflow/dags/crossplane_spark_app_chart_in_cluster_1.py
@@ -23,45 +23,45 @@ dag = DAG(
     tags=['pod','inside','bitnami','crossplane']
 )
 
-spark_app_name="crossplane-airflow"
-Variable.set("spark_app_name", spark_app_name)
+crossplane_app_name="crossplane-airflow"
+Variable.set("crossplane_app_name", crossplane_app_name)
 
-spark_app_namespace="spark-operator"
-Variable.set("spark_app_namespace", spark_app_namespace)
+crossplane_app_namespace="spark-operator"
+Variable.set("crossplane_app_namespace", crossplane_app_namespace)
 
 start_crossplane_pod = KubernetesPodOperator(
     task_id="deploy_helm_crossplane_pod",
-    namespace=spark_app_namespace,
+    namespace=crossplane_app_namespace,
     image="alpine/helm",
-    cmds=['/bin/sh', '-c', 'helm repo add poc https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/helm/packages/ && helm repo update && helm upgrade -i {{ var.value.spark_app_name }} poc/crossplane-service-account --atomic --debug -f https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/airflow/values-crossplane.yaml --set-string fullnameOverride={{ var.value.spark_app_name }}'],
+    cmds=['/bin/sh', '-c', 'helm repo add poc https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/helm/packages/ && helm repo update && helm upgrade -i {{ var.value.crossplane_app_name }} poc/crossplane-service-account --atomic --debug -f https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/airflow/values-crossplane.yaml --set-string fullnameOverride={{ var.value.crossplane_app_name }}'],
     service_account_name="helm-identity",
     do_xcom_push=False,
     is_delete_operator_pod=True,
     get_logs=True,
     log_events_on_failure=True,
-    random_name_suffix=spark_app_name,
+    random_name_suffix=crossplane_app_name,
     kubernetes_conn_id="kubernetes_default",
     dag=dag
 )
 
 start_spark_pod = KubernetesPodOperator(
     task_id="deploy_helm_spark_pod",
-    namespace=spark_app_namespace,
+    namespace=crossplane_app_namespace,
     image="alpine/helm",
-    cmds=['/bin/sh', '-c', 'helm repo add poc https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/helm/packages/ && helm repo update && helm upgrade -i {{ var.value.spark_app_name }} poc/spark-application --atomic --debug -f https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/airflow/values-spark-app.yaml --set-string name={{ var.value.spark_app_name }} --set-string driver.serviceAccount={{ var.value.spark_app_name }} --set-string executor.serviceAccount={{ var.value.spark_app_name }} --set-string fullnameOverride={{ var.value.spark_app_name }}'],
+    cmds=['/bin/sh', '-c', 'helm repo add poc https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/helm/packages/ && helm repo update && helm upgrade -i {{ var.value.crossplane_app_name }} poc/spark-application --atomic --debug -f https://raw.githubusercontent.com/ops-guru/biocatch-poc-dependencies/main/airflow/values-spark-app.yaml --set-string name={{ var.value.crossplane_app_name }} --set-string driver.serviceAccount={{ var.value.crossplane_app_name }} --set-string executor.serviceAccount={{ var.value.crossplane_app_name }} --set-string fullnameOverride={{ var.value.crossplane_app_name }}'],
     service_account_name="helm-identity",
     do_xcom_push=False,
     is_delete_operator_pod=True,
     get_logs=True,
     log_events_on_failure=True,
-    random_name_suffix=spark_app_name,
+    random_name_suffix=crossplane_app_name,
     kubernetes_conn_id="kubernetes_default",
     dag=dag
 )
 sensor = SparkKubernetesSensor(
     task_id='sensor_helm_app',
-    namespace=spark_app_namespace,
-    application_name=spark_app_name,
+    namespace=crossplane_app_namespace,
+    application_name=crossplane_app_name,
     kubernetes_conn_id="kubernetes_default",
     dag=dag,
     api_group="sparkoperator.k8s.io",
